700012b760ececf29b69ca46819f27301fb13a39
Aaron Lu <aaron_lu1@yahoo.com>
2020-01-14 15:39:40 +0800

    Updates

---
+++ precache-manifest.a8f770ef53bed9c45fbdb2c2e4221e5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e05208f4012a863d3d11fd5dabcb8c5",
+    "url": "/react-redux-songs/index.html"
+  },
+  {
+    "revision": "97d0f064b7e42e644773",
+    "url": "/react-redux-songs/static/js/2.f1494e4c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-redux-songs/static/js/2.f1494e4c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e173a9915f62c43a970e",
+    "url": "/react-redux-songs/static/js/main.6c78c1cf.chunk.js"
+  },
+  {
+    "revision": "33e658cce2f3f107101f",
+    "url": "/react-redux-songs/static/js/runtime-main.7d8d3511.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-redux-songs/precache-manifest.a8f770ef53bed9c45fbdb2c2e4221e5e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-redux-songs/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
